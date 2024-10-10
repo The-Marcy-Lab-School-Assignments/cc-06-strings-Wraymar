be48e9d5722816d983cc3803305d102c189490df
Wraymar <tyreesewray14@gmail.com>
2024-10-10 11:05:24 -0400

    finished q1, q2 dosen't work

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //write your code here
+//Q1
+const reverseString = (str) => {
+  let reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
+};
+//console.log(reverseString("hello"));
+
+//Q2
+const reverseZigZag = (str) => {
+  let output = "";
+  for (let i = str.length - 1; i >= 0; i -= 2) {
+    output += str[i].toUpperCase();
+  }
+  for (let i = str.length - 2; i >= 0; i -= 1) {
+    output + str[i];
+  }
+  return output;
+};
+
+//console.log(reverseZigZag("hello"));
